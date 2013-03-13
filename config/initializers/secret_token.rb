3bde6f185e12d8861d91c9c76af881f05d71e52e
Adam Manzanares <nmtadam@gmail.com>
2013-03-13 14:09:28 -0700

    First commit. Rails + TwitterBootstrap Success
    Now that this is going I have to try to create something useful
    and also migrate this to my ec2 instance.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ph4tdsite::Application.config.secret_token = '153a536d2efeec2eb4ad5c534e6a5c996f414951a78463315e9385e6a86118cb703c3c1a97a08119fd6c929624955241517fae6c2215264a74adafb9017e2ecf'
