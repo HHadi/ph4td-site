2872465c9e5ef9fb0593269214f1d0554b4c3ac4
Adam Manzanares <nmtadam@gmail.com>
2013-03-13 16:06:31 -0700

    Added a welcome page.
    This is not up to snuff just yet, but it is something.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,3 @@
 class Trip < ActiveRecord::Base
-  attr_accessible :id, :name
+  attr_accessible :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ph4tdsite::Application.routes.draw do
+  get "welcome/index"
+
   resources :trips
 
 
@@ -51,7 +53,7 @@ Ph4tdsite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
