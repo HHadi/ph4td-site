a30e283a960c1d567f708a529c74c46d0d856959
Adam Manzanares <nmtadam@gmail.com>
2013-03-18 14:56:28 -0700

    Playing with titles again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Improvement over first iteration of the title
+  def full_title(title)
+    base_title = 'Photohacking'
+    if title.empty?
+      base_title
+    else
+      "#{base_title} | #{title}"
+    end
+  end
 end
