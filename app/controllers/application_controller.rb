042d6824e56ba8e517b48228f9132d52900ad031
Adam Manzanares <nmtadam@gmail.com>
2013-03-19 19:53:56 -0700

    Basic sign in and sign out support complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  # Prevent CSRF attack do some googling
+  def handle_unverified_request
+    sign_out
+    super
+  end
+
 end
