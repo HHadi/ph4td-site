91a49e417d857a9d5436e96e45dbdc7c6ba747d7
Adam Manzanares <nmtadam@gmail.com>
2013-03-23 17:01:09 -0700

    Added thumbnail support and photo delete

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :description, :lat, :lng, :url, :image
+  attr_accessible :description, :lat, :lng, :url, :image, :remove_image
   belongs_to :trip
   mount_uploader :image, ImageUploader
   
